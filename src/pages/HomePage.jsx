cac39c21ba633abfccf68bf1f150b1bad86c9180
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-14 15:37:33 +0100

    lista film

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+
 export default function HomePage() {
-    return(<h1>Homepage </h1>)
-   }
-   
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/movies`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Homepage</h1>
+
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </>
+  );
+}
