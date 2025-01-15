fe866578ff7e59d6dd73d394d4f086c580f8bc28
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-15 22:37:23 +0100

    added stars

--- src/components/reviews/ReviewItem.jsx
+++ src/components/reviews/ReviewItem.jsx
@@ -1,9 +1,23 @@
 export default function ReviewItem({ review }) {
-    return (
-      <div className="col-12">
-        <div>{review.name}</div>
-        <div>{review.vote}</div>
-        <div>{review.text}</div>
-      </div>
-    );
-  }
+  const generateStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      const star =
+        i < rating ? (
+          <i className="fa-solid fa-star" key={i}></i>
+        ) : (
+          <i className="fa-regular fa-star" key={i}></i>
+        );
+      stars.push(star);
+    }
+    return stars;
+  };
+
+  return (
+    <div className="col-12">
+      <strong>Review by {review.name}:</strong>
+      <div>Vote: {generateStars(review.vote).map((star) => star)}</div>
+      <div>{review.text}</div>
+    </div>
+  );
+}
